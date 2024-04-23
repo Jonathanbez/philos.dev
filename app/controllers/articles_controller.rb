53d96cfce2ce7ab6f00f5209b41a7fa73c8f9974
Jonathanbez <jbbarroz@gmail.com>
2024-04-23 07:39:20 -0300

    Learning Ruby on Rails / Create a blog / data-turbo-action data-turbo-confirm

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,11 +24,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
 
     if @article.update(article_params)
       redirect_to @article
@@ -36,7 +36,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path
@@ -46,5 +45,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
